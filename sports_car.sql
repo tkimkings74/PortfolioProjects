45ea36a4209e7b461f86ef722f2697825ceddaa2
Anthony Kim <71421380+tkimkings74@users.noreply.github.com>
2023-04-18 11:41:57 -0400

    Update sports_car.sql

--- sports_car.sql
+++ sports_car.sql
@@ -30,92 +30,92 @@ IGNORE 1 ROWS;
 
 -- certain models have incorrect horsepower values such as 1000+ 
 -- we first clean the data to fix these values
-update sports_car 
-set horsepower = substring_index(horsepower, '+', 1)
-where substring(horsepower, length(horsepower),length(horsepower)) = '+'; 
+UPDATE sports_car 
+SET horsepower = substring_index(horsepower, '+', 1)
+WHERE substring(horsepower, length(horsepower),length(horsepower)) = '+'; 
 
 -- some horsepower values have a comma in them. we fix this 
 
-select * from sports_car
-where horsepower like '%,%'; 
+SELECT * FROM sports_car
+WHERE horsepower LIKE '%,%'; 
 
 
 
 -- remove comma from horsepower value that contains comma 
-update sports_car 
-set horsepower = substring_index(horsepower, ',', 1) + substring_index(horsepower, ',', -1)
-where horsepower like '%,%'; 
+UPDATE sports_car 
+SET horsepower = substring_index(horsepower, ',', 1) + substring_index(horsepower, ',', -1)
+WHERE horsepower LIKE '%,%'; 
 
 
-update sports_car 
-set horsepower = convert(horsepower, double);
+UPDATE sports_car 
+SET horsepower = convert(horsepower, double);
 
-alter table sports_car 
-add horse_power double; 
+ALTER TABLE sports_car 
+ADD horse_power double; 
 
-update sports_car 
-set horse_power = convert(horsepower, double); 
+UPDATE sports_car 
+SET horse_power = convert(horsepower, double); 
 
 
 
 -- we find an outlier Tesla model with horsepower of 10000, which is clearly an input error. we fix the horsepower 
 
-update sports_car 
-set horsepower = 1000 
-where car_make = 'tesla' 
-and car_model = 'roadster'
-and horsepower > 5000;
+UPDATE sports_car 
+SET horsepower = 1000 
+WHERE car_make = 'tesla' 
+AND car_model = 'roadster'
+AND horsepower > 5000;
 
 -- some data records have incorrect horsepower value where two identical models with the same year and make have different values. 
 -- we correct this by using the larger value of the two using selfjoin. 
 
 
-update sports_car 
-as a join sports_car as b 
-on a.car_make = b.car_make
-and a.car_model = b.car_model 
-and a.year = b.year 
-and a.horsepower != b.horsepower
-set a.horsepower = greatest(a.horsepower, b.horsepower);
+UPDATE sports_car 
+AS a JOIN sports_car AS b 
+ON a.car_make = b.car_make
+AND a.car_model = b.car_model 
+AND a.year = b.year 
+AND a.horsepower != b.horsepower
+SET a.horsepower = greatest(a.horsepower, b.horsepower);
 
 
 
-update sports_car 
-as a join sports_car as b 
-on a.car_make = b.car_make
-and a.car_model = b.car_model 
-and a.year = b.year 
-and a.horsepower != b.horsepower
-set b.horsepower = greatest(a.horsepower, b.horsepower);
+UPDATE sports_car 
+AS a JOIN sports_car AS b 
+ON a.car_make = b.car_make
+AND a.car_model = b.car_model 
+AND a.year = b.year 
+AND a.horsepower != b.horsepower
+SET b.horsepower = greatest(a.horsepower, b.horsepower);
 
 
 -- price field contains comma, which we want removed. 
 
-alter table sports_car
-add price_ double; 
+ALTER TABLE sports_car
+ADD price_ double; 
 
 
-update sports_car 
-set price_ =
-case 
-	when length(price) <= 7 then convert(concat(substring_index(price, ',', 1), substring_index(price, ',', -1)), double)
-    when length(price) > 7 then convert(concat(substring_index(price, ',', 1), substring_index(substring_index(price, ',', 2), ',', -1), 
+UPDATE sports_car 
+SET price_ =
+CASE 
+	WHEN length(price) <= 7 THEN convert(concat(substring_index(price, ',', 1), substring_index(price, ',', -1)), double)
+	WHEN length(price) > 7 THEN convert(concat(substring_index(price, ',', 1), substring_index(substring_index(price, ',', 2), ',', -1), 
 					substring_index(price, ',', -1)), double)
-	else price
-    end;
+	ELSE price
+	END;
 
-alter table sports_car 
-drop column price; 
+ALTER TABLE sports_car 
+DROP column price; 
 
-alter table sports_car
-rename column price_ to price; 
+ALTER TABLE sports_car
+RENAME column price_ to price; 
 
 
 
 -- delete tesla vehicles with horsepower input error
-delete from sports_car 
-where car_make = 'tesla' 
-and horsepower <= 500; 
+DELETE FROM sports_car 
+WHERE car_make = 'tesla' 
+AND horsepower <= 500; 
 
 
 
@@ -123,124 +123,125 @@ and horsepower <= 500;
 -- fix engine_size values: electric vehicles and hybrids have values other than numbers
 SELECT * FROM sports_Car WHERE concat('',convert(engine_size, double) * 1) != engine_size;
 
-update sports_car
-set engine_size = 'electric' 
-where engine_size like '%electric%';
+UPDATE sports_car
+SET engine_size = 'electric' 
+WHERE engine_size LIKE '%electric%';
 
-update sports_car
-set engine_size = 'hybrid' 
-where engine_size like '%hybrid%';
+UPDATE sports_car
+SET engine_size = 'hybrid' 
+WHERE engine_size LIKE '%hybrid%';
 
-update sports_car
-set engine_size = 'electric' 
-where engine_size like '%n/a%';
+UPDATE sports_car
+SET engine_size = 'electric' 
+WHERE engine_size LIKE '%n/a%';
 
 
-update sports_car
-set engine_size = 'electric' 
-where engine_size like '%-%';
+UPDATE sports_car
+SET engine_size = 'electric' 
+WHERE engine_size LIKE '%-%';
 
-update sports_car
-set engine_size = 'electric' 
-where engine_size like '0';
+UPDATE sports_car
+SET engine_size = 'electric' 
+WHERE engine_size LIKE '0';
 
 
 -- fix torque values 
 
-select distinct torque from sports_car order by torque; 
+SELECT DISTINCT torque FROM sports_car ORDER BY torque; 
 
-select * from sports_car where torque like '%+%';
-
-
-
-update sports_car 
-set torque = 7376 
-where car_make = 'tesla' 
-and car_model = 'roadster';  
-
-update sports_car 
-set torque = 1020 
-where car_model = 'model s plaid' 
-and torque = 'n/a';
-
-update sports_car 
-set torque = 520 
-where car_make = 'maserati'
-and car_model = 'granturismo';
-
-alter table sports_car 
-drop column electric; 
-
-select * from sports_car 
-where torque = 'n/a' ;
-
-
-alter table sports_car 
-add torque_ double ;
-
-update sports_car 
-set torque = 
-case when torque like '%,%' then concat(substring_index(torque, ',', 1), substring_index(torque, ',', -1))
-	else torque
-    end; 
-
-
-alter table sports_car 
-modify torque double; 
-
-select * from sports_car 
-where torque like '%,%'; 
-
-alter table sports_car 
-modify zero_to_60 double; 
+SELECT * FROM sports_car WHERE torque LIKE '%+%';
 
 
 
+UPDATE sports_car 
+SET torque = 7376 
+WHERE car_make = 'tesla' 
+AND car_model = 'roadster';  
+
+UPDATE sports_car 
+SET torque = 1020 
+WHERE car_model = 'model s plaid' 
+AND torque = 'n/a';
+
+UPDATE sports_car 
+SET torque = 520 
+WHERE car_make = 'maserati'
+AND car_model = 'granturismo';
+
+ALTER TABLE sports_car 
+DROP column electric; 
+
+SELECT * FROM sports_car 
+WHERE torque = 'n/a' ;
 
 
-create table car_data as
-with cte as (
-	select * from sports_car 
-) select * from cte; 
+ALTER TABLE sports_car 
+ADD torque_ double ;
+
+UPDATE sports_car 
+SET torque = 
+CASE 
+	WHEN torque LIKE '%,%' THEN concat(substring_index(torque, ',', 1), substring_index(torque, ',', -1))
+	ELSE torque
+	END; 
 
 
-select * from car_data; 
+ALTER TABLE sports_car 
+MODIFY torque double; 
 
+SELECT * FROM sports_car 
+WHERE torque LIKE '%,%'; 
 
-select car_make, car_model, min(zero_to_60) as pace
-from sports_car 
-group by car_make, car_model
-order by pace ;
-
-
-select * from car_data as a
-where zero_to_60 = (select  min(zero_to_60)from car_data as b where a.car_make = b.car_make)
-order by zero_to_60, car_make;
-
-select car_make,  min(zero_to_60) as acceleration from car_data as a
-where zero_to_60 = (select  min(zero_to_60)from car_data as b where a.car_make = b.car_make)
-group by car_make 
-order by acceleration, car_make;
-
-create table brand_by_acceleration as 
-with cte as (select car_make,  min(zero_to_60) as acceleration from car_data as a
-where zero_to_60 = (select  min(zero_to_60)from car_data as b where a.car_make = b.car_make)
-group by car_make 
-order by acceleration, car_make) select * from cte; 
+ALTER TABLE sports_car 
+MODIFY zero_to_60 double; 
 
 
 
-select car_make, car_model, year, avg(horsepower), avg(zero_to_60) as pace, 
-avg(torque) from car_data 
-group by car_make, car_model, year
-order by pace 
-limit 15;
 
-select car_make, car_model, avg(horsepower) as horsepower, avg(zero_to_60) as acceleration, 
-avg(torque) as torque from car_data 
-group by car_make, car_model
-order by acceleration 
-limit 15;
+
+CREATE TABLE car_data AS
+with cte AS (
+	SELECT * FROM sports_car 
+) SELECT * FROM cte; 
+
+
+
+
+
+SELECT car_make, car_model, min(zero_to_60) AS pace
+FROM sports_car 
+GROUP BY car_make, car_model
+ORDER BY pace ;
+
+
+SELECT * FROM car_data AS a
+WHERE zero_to_60 = (SELECT  min(zero_to_60) FROM car_data AS b WHERE a.car_make = b.car_make)
+ORDER BY zero_to_60, car_make;
+
+SELECT car_make,  min(zero_to_60) AS acceleration FROM car_data AS a
+WHERE zero_to_60 = (SELECT  min(zero_to_60)FROM car_data AS b WHERE a.car_make = b.car_make)
+GROUP BY car_make 
+ORDER BY acceleration, car_make;
+
+CREATE TABLE brand_by_acceleration AS 
+with cte AS (SELECT car_make,  min(zero_to_60) AS acceleration FROM car_data AS a
+WHERE zero_to_60 = (SELECT  min(zero_to_60) FROM car_data AS b WHERE a.car_make = b.car_make)
+GROUP BY car_make 
+GROUP BY acceleration, car_make) SELECT * FROM cte; 
+
+
+
+SELECT car_make, car_model, year, avg(horsepower), avg(zero_to_60) AS pace, 
+avg(torque) FROM car_data 
+GROUP BY car_make, car_model, year
+ORDER BY pace 
+LIMIT 15;
+
+SELECT car_make, car_model, avg(horsepower) AS horsepower, avg(zero_to_60) AS acceleration, 
+avg(torque) AS torque FROM car_data 
+GROUP BY car_make, car_model
+ORDER BY acceleration 
+LIMIT 15;
 
 
 
